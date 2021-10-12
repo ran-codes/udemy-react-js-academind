230740b424ef7247390b7d46b00e4ff4e29ced40
rl627 <rl627@drexel.edu>
2021-10-12 14:16:56 -0400

    Section 8 RL: Modal component and logic

--- project3-complete-practice-prj/src/App.js
+++ project3-complete-practice-prj/src/App.js
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import NewUserInput from './components/NewUser/NewUserInput/NewUserInput';
+import InvalidModal from './components/NewUser/InvalidModal/InvalidModal';
+
 
 import './App.css';
 
 const App = () => {
-   return (
-     <div className = "app">
-       <NewUserInput />
-     </div>
-   );
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+
+  const submitHandler = (invalidationState) => {
+    setModalMessage(invalidationState);
+    if (invalidationState !== '') { setShowModal(true); }
+  }
+
+  const closeModalHandler = () => {
+    console.log('CLICKED CLOSE');
+    setShowModal(false);
+  }
+
+  return (
+    <div className="app">
+      <NewUserInput onSubmit={submitHandler} />
+      <InvalidModal
+        show={showModal}
+        message={modalMessage}
+        onClose={closeModalHandler}
+      />
+    </div>
+  );
 };
 
 export default App;
--- project3-complete-practice-prj/src/components/NewUser/NewUserInput/NewUserInput.js
+++ project3-complete-practice-prj/src/components/NewUser/NewUserInput/NewUserInput.js
@@ -1,25 +1,63 @@
+import { useState } from 'react';
 import Card from '../../UI/Cards';
 import Button from '../../UI/Button';
 
 import styles from './NewUserInput.module.css';
 
-const NewUserInput = () => {
+const NewUserInput = (props) => {
+  const [userName, setUserName] = useState('');
+  const [userAge, setUserAge] = useState('');
+
+  const onUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const onUserAgeChange = (event) => {
+    setUserAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const errorMissing = userName.length === 0 || userAge.length === 0;
+    const errorInvalidAge = userAge < 0;
+    let invalidationMessage = ""
+
+    if (errorMissing) {
+      invalidationMessage='Please enter a valid name and age (non-empty values).';
+    } else if (errorInvalidAge) {
+      invalidationMessage = 'Please enter a valid age (> 0).';
+    } else {
+      setUserName('');
+      setUserAge('');
+    }
+    props.onSubmit(invalidationMessage);
+  }
+
   return (
     <Card>
-      <form className={styles.controls}>
+      <form className={styles.controls} onSubmit={submitHandler}>
         <div className={styles.control}>
           <label>Username</label>
-          <input type="text" name="username"></input>
+          <input
+            type="text"
+            name="username"
+            value={userName}
+            onChange={onUserNameChange}
+          ></input>
         </div>
         <div className={styles.control}>
           <label>Age (Years)</label>
-          <input type="number" name="age"></input>
+          <input
+            type="number"
+            name="age"
+            value={userAge}
+            onChange={onUserAgeChange}
+          ></input>
         </div>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
-
-}
+};
 
 export default NewUserInput;
