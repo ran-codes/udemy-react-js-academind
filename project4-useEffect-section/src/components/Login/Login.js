7f0d215b8f033688fe6e69b20566c8086724f711
rl627 <rl627@drexel.edu>
2021-10-18 14:51:01 -0400

    113. useEffect clean up funciton

--- project4-useEffect-section/src/components/Login/Login.js
+++ project4-useEffect-section/src/components/Login/Login.js
@@ -12,23 +12,23 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-   setFormIsValid(
-     enteredEmail.includes('@') && enteredPassword.trim().length > 6
-   );
-  })
+    setTimeout(() => {
+       setFormIsValid(
+         enteredEmail.includes('@') && enteredPassword.trim().length > 6
+       );
+    }, 500)
+
+    return (() => {
+
+    })
+  }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
